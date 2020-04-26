5d27ab445044c54920af23d4820937249af54c3c
Illia Halych <middlezwei@gmail.com>
2020-04-26 19:07:24 +0200

    Models: Venue, Artist, Show
    - Venue and Artist - parent tables
    - Show - associative table
    - added representational method of an object

--- app.py
+++ app.py
@@ -36,6 +36,63 @@ if not database_exists(engine.url):
     create_database(engine.url)
 
 
+# ----------------------------------------------------------------------------#
+# Models.
+# ----------------------------------------------------------------------------#
+
+
+class Show(db.Model):
+    __tablename__ = 'show'
+
+    id = db.Column(db.Integer, primary_key=True)
+    venue_id = db.Column(db.Integer, db.ForeignKey('venue.id'))
+    artist_id = db.Column(db.Integer, db.ForeignKey('artist.id'))
+    start_time = db.Column(db.DateTime(timezone=True))
+
+    def __repr__(self):
+        return f'<Show {self.id}>'
+
+
+class Venue(db.Model):
+    __tablename__ = 'venue'
+    __table_args__ = {'extend_existing': True}
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    address = db.Column(db.String(120), nullable=False)
+    genres = db.Column(db.String)
+    phone = db.Column(db.String(120))
+    image_link = db.Column(db.String(500), nullable=False)
+    facebook_link = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean, nullable=False, default=False)
+    website = db.Column(db.String(120))
+    shows = db.relationship("Show", cascade="all,delete", backref="venue")
+
+    def __repr__(self):
+        return f'<Venue {self.id} {self.name}>'
+
+
+class Artist(db.Model):
+    __tablename__ = 'artist'
+    __table_args__ = {'extend_existing': True}
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120))
+    genres = db.Column(db.String)
+    image_link = db.Column(db.String(500), nullable=False)
+    facebook_link = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean, nullable=False, default=False)
+    website = db.Column(db.String(120))
+    shows = db.relationship("Show", cascade="all,delete", backref="artist")
+
+    def __repr__(self):
+        return f'<Artist {self.id} {self.name}>'
+
 
 # ----------------------------------------------------------------------------#
 # Launch.
