c1b8c17886cda9e1793519bf2bceb8b203de9a0b
Illia Halych <middlezwei@gmail.com>
2020-04-26 19:38:19 +0200

    Error handlers
    - Takes us to error pages
    - Supports 404, 500

--- app.py
+++ app.py
@@ -513,6 +513,16 @@ def create_show_submission():
     return render_template('pages/home.html')
 
 
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('errors/404.html'), 404
+
+
+@app.errorhandler(500)
+def server_error(error):
+    return render_template('errors/500.html'), 500
+
+
 # ----------------------------------------------------------------------------#
 # Launch.
 # ----------------------------------------------------------------------------#
