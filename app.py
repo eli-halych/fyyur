46ea2996f7656dae081ff288800a11c40658c0c9
Illia Halych <middlezwei@gmail.com>
2020-04-26 19:14:22 +0200

    Format datetime for frontend (Jinja)
    - Parse full date and time
    - Parse shortened date and time

--- app.py
+++ app.py
@@ -37,6 +37,26 @@ if not database_exists(engine.url):
 
 
 # ----------------------------------------------------------------------------#
+# Filters.
+# ----------------------------------------------------------------------------#
+
+def format_datetime(value, format='medium'):
+    if type(value) != datetime:
+        date = dateutil.parser.parse(value)
+    else:
+        date = value
+
+    if format == 'full':
+        format = "EEEE MMMM, d, y 'at' h:mma"
+    elif format == 'medium':
+        format = "EE MM, dd, y h:mma"
+    return babel.dates.format_datetime(date, format, locale='en')
+
+
+app.jinja_env.filters['datetime'] = format_datetime
+
+
+# ----------------------------------------------------------------------------#
 # Models.
 # ----------------------------------------------------------------------------#
 
